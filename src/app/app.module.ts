83cc6e47dd51353213db960b2b706d3b9866b1d4
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-02 22:01:15 -0300

    Estruturação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SobreComponent,
+    ExperienciaComponent,
+    ProjetosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
