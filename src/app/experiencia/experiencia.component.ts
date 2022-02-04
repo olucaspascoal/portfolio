01a7cacc08ba329c858bc2426aa56d15da779d55
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-03 22:11:28 -0300

    Desenvolvimento do component experiencia

--- src/app/experiencia/experiencia.component.ts
+++ src/app/experiencia/experiencia.component.ts
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ExperienciaComponent implements OnInit {
 
   constructor() { }
-
+  public experiencias: any;
   ngOnInit(): void {
+    this.experiencias = [
+      {'nome': 'Digital House', 'tempo': 'Nov 2021 - Atual', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'},
+      {'nome': 'Código Fonte TV', 'tempo': 'Nov 2021 - Atual', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'},
+      {'nome': 'Zuplae', 'tempo': 'Nov 2021 - Atual', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'},
+      {'nome': 'ContWeb', 'tempo': 'Nov 2021 - Atual', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'},
+      {'nome': 'iurecode', 'tempo': 'Nov 2021 - Atual', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'},
+      {'nome': 'Freelancer', 'tempo': 'Nov 2021 - Atual', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna.'},
+    ]
   }
 
 }
